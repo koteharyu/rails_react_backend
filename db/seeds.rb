66846055448594317084fb0ce063131e57b0c989
haryu <hryau6@gmail.com>
2021-10-09 13:42:33 +0900

    add: seed of body

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-25.times do |i|
-  Post.create!(title: "#{i}_title", content: "#{i}_content")
-  p "created #{i} post"
+10.times do |i|
+  weight = 60 + i
+  Body.create!(weight: weight)
 end
